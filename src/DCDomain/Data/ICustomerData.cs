838e30c3871b0f130903a36e69fd010c9d4b473e
JOAO PAULO <joao.silva@mgmcorp.com.br>
2021-04-06 15:46:18 -0300

    Add Interface Data Domain

---
+++ src/DCDomain/Data/ICustomerData.cs
@@ -0,0 +1,18 @@
+﻿using DCDomain.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DCDomain.Data
+{
+    public interface ICustomerData : IDisposable
+    {
+        Task GetById(Guid id);
+        Task<IEnumerable<Customer>> GetAll();
+        Task<Customer> GetByEmail(string email);
+
+        void Add(Customer customer);
+        void Update(Customer customer);
+        void Remove(Customer customer);
+    }
+}
